f5041426d8cc051bb5ac5c7aeec727006602a71b
Demontier <demontier@outlook.com>
2022-02-04 23:34:59 -0300

    refatoraçao header

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const button = document.querySelector(".header_btn");
-const navbar = document.querySelector(".header_navbar");
+const navbar = document.querySelector(".navbar");
+const animated = document.querySelectorAll(".animated");
 
 
 const toggleMenu = () => {
   if( event.type === 'touchstart') event.preventDefault();
   navbar.classList.toggle("toggle")
+  for (let index = 0; index < animated.length; index += 1) {
+    animated[index].classList.toggle("animated")
+  }
 }
 
 button.addEventListener('click', toggleMenu)
