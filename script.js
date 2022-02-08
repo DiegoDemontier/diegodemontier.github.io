35eccdc4b41fa5065493a535a6cc2e45759dd53a
Demontier <demontier@outlook.com>
2022-02-08 18:42:42 -0300

    section slide

--- script.js
+++ script.js
@@ -11,6 +11,49 @@ const toggleMenu = () => {
   }
 }
 
-button.addEventListener('click', toggleMenu)
-button.addEventListener('click', toggleMenu)
+/* var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+}); */
 
+button.addEventListener('click', toggleMenu)
+button.addEventListener('touchstart', toggleMenu)
+
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  loop: true
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  loop: true
+});
